767177036503ee22d0d92383261fbecabe8c933d
Paola Garcia <paoga.87@gmail.com>
2016-06-20 13:41:01 -0400

    Update and rename B2LRcalculatorECS.js to src/B2LRcalculatorECS.js

--- src/B2LRcalculatorECS.js
+++ src/B2LRcalculatorECS.js
@@ -1,7 +1,7 @@
-// This Calculator was created
+// This JSCalculator was created
 // by Paola Garcia C. for
 // ECSprofessional.com
-//(c)2012 All Rights Reserved.
+// (c)2012 All Rights Reserved.
 // If you would like to use it on your website, 
 // please contact: info@ecsprofessional.com
 
